78ca60825d2a3ac6f9d4adc14b1865e6e1dcd42a
atto08 <atto08@naver.com>
2022-10-18 00:16:42 +0900

    login

---
+++ assign22/src/main/java/com/sparta/assign22/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.sparta.assign22.controller;
+
+import com.sparta.assign22.entity.UserRoleEnum;
+import com.sparta.assign22.security.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("username", userDetails.getUsername());
+
+        if (userDetails.getUser().getRole() == UserRoleEnum.ADMIN) {
+            model.addAttribute("admin_role", true);
+        }
+
+        return "index";
+    }
+}
